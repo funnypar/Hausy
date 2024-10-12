8e909730ad5c5a7bb10362f337c61f12269ded56
Mopano <p.r.7.norouzi@gmail.com>
2024-10-12 05:26:03 -0700

    feature: added Properties Page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,27 @@
+import PropertyCard from "@/components/PropertyCard";
+import properties from "@/properties.json";
+
 const ProperitesPage = () => {
-    return <div>PropertiesPage</div>;
+    return (
+        <section className="px-4 py-6">
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                {properties.length === 0 ? (
+                    <p>No Properties Found!</p>
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {properties.map((property) => {
+                            return (
+                                <PropertyCard
+                                    key={property._id}
+                                    property={property}
+                                />
+                            );
+                        })}
+                    </div>
+                )}
+            </div>
+        </section>
+    );
 };
 
 export default ProperitesPage;
