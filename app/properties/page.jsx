2eefe5c108e248467b03dbc03c2b7b0ee4dea644
Mopano <p.r.7.norouzi@gmail.com>
2024-10-16 01:32:31 -0700

    feature: added Supabase database

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,7 +1,9 @@
 import PropertyCard from "@/components/PropertyCard";
-import properties from "@/properties.json";
+import { getAllProperties } from "@/utils/services/getAllProperties";
 
-const ProperitesPage = () => {
+const ProperitesPage = async () => {
+    const properties = await getAllProperties();
+
     return (
         <section className="px-4 py-6">
             <div className="container-xl lg:container m-auto px-4 py-6">
@@ -9,10 +11,10 @@ const ProperitesPage = () => {
                     <p>No Properties Found!</p>
                 ) : (
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                        {properties.map((property) => {
+                        {properties?.map((property) => {
                             return (
                                 <PropertyCard
-                                    key={property._id}
+                                    key={property.id}
                                     property={property}
                                 />
                             );
