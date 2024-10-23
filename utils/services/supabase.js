b4afd6d24594863375df81ea22c6aee130fe489a
Mopano <p.r.7.norouzi@gmail.com>
2024-10-22 23:59:14 -0700

    fixBug: added Supabase in authOptions

--- utils/services/supabase.js
+++ utils/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://juyibhfuuvdjptkpenxp.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1eWliaGZ1dXZkanB0a3BlbnhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkwNTgzNTcsImV4cCI6MjA0NDYzNDM1N30.qsKOuqBRJ0XvkeUsQ7GRNogQGrgi0BOrLtfJGFbDusE";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
