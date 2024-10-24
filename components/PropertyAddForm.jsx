737b449c89b04700d76b346a7319d0abbe7af1f4
Mopano <p.r.7.norouzi@gmail.com>
2024-10-24 12:23:09 -0700

    fixBug : added Use Effect source

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const PropertyAddForm = () => {
     const [mounted, setMounted] = useState(false);
